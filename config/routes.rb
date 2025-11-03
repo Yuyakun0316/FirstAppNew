2f4881d0a2f7ca7efc9f370ab086b730dd33d4e5
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-03 11:46:26 +0900

    root 'posts#index'を削除してしまっていた為

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # ↓ この行を追加、またはコメントアウトを解除して編集します
+  root 'posts#index'
+  
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
   get 'posts', to: 'posts#create'
